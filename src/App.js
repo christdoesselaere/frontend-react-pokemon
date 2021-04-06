8079f87c14f3c46576ca6051d38e017084945ee4
Christ Doesselaere <67807770+christdoesselaere@users.noreply.github.com>
2021-04-06 22:53:15 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Pokedex from "./components/pokedex/Pokedex";
 import headerLogo from './assets/pokedex_logo.webp';
+import Footer from "./components/footer/Footer";
 
 function App() {
     return (
         <>
             <img className="header" id="header-logo" src={headerLogo} alt="pokedex-logo"/>
             <Pokedex/>
+            <Footer/>
         </>
     );
 }
